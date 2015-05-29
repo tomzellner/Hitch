773496f027a023358f69ae9833fdd3e3422c78b7
Tom Zellner <tom.zellner@gmail.com>
2015-05-28 22:06:47 -0700

    finished all associations

--- hitch_backend/app/models/user.rb
+++ hitch_backend/app/models/user.rb
@@ -7,11 +7,25 @@ class User < ActiveRecord::Base
         has_many :cars
 
         has_many :drivers, through: :driver_trips, source: :driver
-         ##VVV not sure below
         has_many :driver_trips, foreign_key: :driver_id, class_name: 'Trip'
 
         has_many :passengers, through: :passenger_riders
         has_many :passenger_riders, foreign_key: :passenger_id, class_name: 'Rider'
+
+
+
+
+        has_many :raters, through: :rater_ratings, source: :rater
+        has_many :rater_ratings, foreign_key: :rated_id, class_name: 'Rating'
+
+        has_many :rateds, through: :rated_ratings, source: :rated
+        has_many :rated_ratings, foreign_key: :rater_id, class_name: 'Rating'
+
+
+
+
+
+
 end
 
 
--- hitch_backend/db/schema.rb
+++ hitch_backend/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529013642) do
+ActiveRecord::Schema.define(version: 20150529041111) do
 
   create_table "cars", force: :cascade do |t|
     t.integer  "seats"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20150529013642) do
     t.string   "type"
     t.integer  "year"
     t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "rater_id"
+    t.integer  "rated_id"
+    t.integer  "score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
