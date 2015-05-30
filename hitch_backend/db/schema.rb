27aa53ebfac5d92e9aea5403d57f33895ce649cf
JessC <jesse.callwood@gmail.com>
2015-05-29 19:49:14 -0700

    add session serializer folder and file

--- hitch_backend/db/schema.rb
+++ hitch_backend/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529221709) do
+ActiveRecord::Schema.define(version: 20150529234712) do
 
   create_table "cars", force: :cascade do |t|
     t.integer  "seats"
@@ -67,9 +67,10 @@ ActiveRecord::Schema.define(version: 20150529221709) do
     t.string   "first_name"
     t.string   "last_name"
     t.datetime "birthdate"
-    t.string   "phonenumber"
+    t.integer  "phonenumber"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "access_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
